133de8237290da290df78482f47f6d2912be925d
Brian Jenkins <brianjenkins94@gmail.com>
2019-08-23 16:48:04 -0400

    Updated `.eslintrc.json`

--- src/environment/index.ts
+++ src/environment/index.ts
@@ -1,7 +1,7 @@
 import { Browser } from "./browser";
 
 // This is pretty terrible, but I can't find a better way to do it.
-const Environment = typeof(process) !== "undefined" ? require("./terminal").Terminal : Browser;
+const Environment = typeof process !== "undefined" ? require("./terminal").Terminal : Browser;
 
 const terminal = new Environment();
 
